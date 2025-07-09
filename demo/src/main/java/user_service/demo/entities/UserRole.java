e9f31b69e8bf3db6facf87eac3225dec895db768
Andrei <djurinschi.andreiai.com>
2025-07-09 16:44:06 +0300

    Init

---
+++ demo/src/main/java/user_service/demo/entities/UserRole.java
@@ -0,0 +1,5 @@
+package user_service.demo.entities;
+
+public enum UserRole {
+    ADMIN, MANAGER, TEACHER
+}
---
+++ demo/src/main/java/user_service/demo/utils/PasswordHash.java
@@ -0,0 +1,6 @@
+package user_service.demo.utils;
+
+public class PasswordHash {
+
+    private static final
+}
